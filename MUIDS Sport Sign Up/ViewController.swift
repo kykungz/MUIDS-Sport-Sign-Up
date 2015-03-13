1eded2ca68f62fa7420c33971eb35c130c808f39
puneetv9 <puneetv@iMac09.local>
2015-03-13 12:13:05 +0700

    Initial Commit

---
+++ MUIDS Sport Sign Up/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MUIDS Sport Sign Up
+//
+//  Created by puneetv9 on 3/13/2558 BE.
+//  Copyright (c) 2558 Kkz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ MUIDS Sport Sign UpTests/MUIDS_Sport_Sign_UpTests.swift
@@ -0,0 +1,36 @@
+//
+//  MUIDS_Sport_Sign_UpTests.swift
+//  MUIDS Sport Sign UpTests
+//
+//  Created by puneetv9 on 3/13/2558 BE.
+//  Copyright (c) 2558 Kkz. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class MUIDS_Sport_Sign_UpTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
